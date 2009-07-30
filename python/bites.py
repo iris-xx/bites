3a95d321d02a4432dfc93f3d2dee5360773f4346
Jeremy R. Fishman <jfishman@bowdoin.edu>
2009-07-30 09:11:03 -0400

    updated Python bites interface
      moved everything into the 'bites' module.  To make the interface
      consistent with the future C++ interface, changed the way the environment
      is created, and how descriptors and tables are accessed.

--- python/bites.py
+++ python/bites.py
@@ -1,20 +1,48 @@
 
 import bsddb.db as db
+import datetime
+import hostname
 import sys
 
-home = None
-env = None
-
-def set_home(path):
-    global home
-    home = str(path)
-
-def init_env():
-    global env, home
-    if not home:
-        raise TypeError('Home directory not set, use set_home(path) first')
-
+def init_env(home):
     env = db.DBEnv()
-    env.open(home, db.DB_INIT_CDB | db.DB_INIT_MPOOL | db.DB_CREATE |
-                   db.DB_RECOVER)
+    env.open(home, db.DB_INIT_CDB | db.DB_INIT_MPOOL | db.DB_CREATE)
+    return env
+
+def tablename(msg):
+    return msg.__class__.DESCRIPTOR.full_name
+
+class ProtoStore(object):
+    def __init__(self, env, prefix=hostname.hostname,
+                            name=None,
+                            suffix=None,
+                            name_factory=datetime.date.today):
+        self.env = env
+
+        if not name:
+            name = name_factory()
+        if suffix:
+            self.path = '.'.join((str(prefix), str(name), str(suffix)))
+        else:
+            self.path = '.'.join((str(prefix), str(name)))
+
+        self.tables = {}
+
+    def init_table(self, name):
+        table = db.DB(self.env)
+        table.open(self.path, name, db.DB_BTREE, db.DB_CREATE)
+        self.tables.setdefault(name, table)
+
+    def get(self, key, msg):
+        name = tablename(msg)
+        if not self.tables.has_key(name):
+            self.init_table(name)
+        msg.ParseFromString(self.tables[name].get(key))
+        return msg
+
+    def put(self, key, msg):
+        name = tablename(msg)
+        if not self.tables.has_key(name):
+            self.init_table(name)
+        self.tables[name].put(key, msg.SerializeToString())
 
